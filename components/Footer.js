6d2f64dc996a81fc4e0bed492bf4c0cbe8a64921
William Price <pricew86@gmail.com>
2022-10-22 16:22:01 +0800

    added module aliases

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
-import styles from "../styles/Footer.module.css";
+import styles from "@/styles/Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.foter}>
       <p>Copyright &copy; DJ Events 2021</p>
       <p>
         <Link href="/about">

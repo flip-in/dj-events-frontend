a9a68aa1a8450398e895fd9ba57e03ee5acccd1c
William Price <pricew86@gmail.com>
2022-10-23 14:51:14 +0800

    styled event page

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,7 +3,6 @@ import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 
 export default function EventsPage({ events }) {
-  console.log(events);
   return (
     <Layout>
       <h1>Events</h1>
